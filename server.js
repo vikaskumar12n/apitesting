d521be21fda7c6b93843634cac70111dd545bf6a
vikaskumar12n <vvikaskumar393@hgmail.com>
2026-04-17 16:21:11 +0530

    update CORS config and prepare for production deployment

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ dotenv.config({ path: path.join(__dirname, ".env") });
 const app=express()
  
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'], // Frontend URLs
+    origin: ['http://localhost:5173', 'http://localhost:3000',
+           'https://rb-frontend-rosy.vercel.app',      
+        'https://rb-frontend-ea4w.vercel.app'
+    ], // Frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
