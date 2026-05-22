30984f8aef65d859716dd8b17d06da6c2a945b53
vikaskumar12n <vvikaskumar393@gmail.com>
2026-05-22 17:26:17 +0530

    add body size

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }))
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api",resumeRoutes)
 app.use("/api",routers)
 app.use("/api",apiRouter)
